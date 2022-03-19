de7a39df09dda1ea02743081d73b1ba9c3c8230e
Marcus <marcusmoraes2010@hotmail.com>
2022-03-19 19:19:22 -0300

    feat: add sign in and sign up screens

--- script.js
+++ script.js
@@ -3,29 +3,70 @@ import setGameUp  from "./game/index.js"
 
 const rootHtmlElement = document.getElementById("root");
 
+renderInicialScren()
 
-const renderGameScreen = () => {
-const mainGameDiv = createHtmlElement("div", "main-game")
+function renderInicialScren (isLogin = true, lastForm) {
+  if(lastForm) rootHtmlElement.removeChild(lastForm)
 
-const gameDiv = createHtmlElement("div", "genius")
+  const signInForm = createHtmlElement("form");
 
-const redOptionDiv = createHtmlElement("div", "red");
-const greenOptionDiv = createHtmlElement("div", "green");
-const blueOptionDiv = createHtmlElement("div", "blue");
-const yellowOptionDiv = createHtmlElement("div", "yellow");
+  const nameInput = createHtmlElement("input");
+  nameInput.setAttribute("type","text")
+  nameInput.setAttribute("placeholder", "nome");
 
-gameDiv.appendChild(redOptionDiv)
-gameDiv.appendChild(greenOptionDiv)
-gameDiv.appendChild(blueOptionDiv)
-gameDiv.appendChild(yellowOptionDiv)
+  const passwordInput = createHtmlElement("input");
+  passwordInput.setAttribute("type","password")
+  passwordInput.setAttribute("placeholder", "senha");
 
-mainGameDiv.appendChild(gameDiv)
-rootHtmlElement.appendChild(mainGameDiv)
+  const submitInput = createHtmlElement("input");
+  submitInput.setAttribute("type","submit")
+  submitInput.setAttribute("value", isLogin? "entrar" : "cadastrar");
+
+  const changeFormButton = createHtmlElement("button");
+  changeFormButton.innerHTML = isLogin? "Ou cadastre-se" : "Ou entre";
+  changeFormButton.addEventListener("click", () =>renderInicialScren(!isLogin, signInForm))
+
+  signInForm.addEventListener("submit", (e) => submitForm(nameInput.value, passwordInput.value, isLogin, e))
+
+  signInForm.appendChild(nameInput)
+  signInForm.appendChild(passwordInput)
+  signInForm.appendChild(submitInput)
+  signInForm.appendChild(changeFormButton)
+
+  rootHtmlElement.appendChild(signInForm)
 }
 
-renderGameScreen()
+function submitForm (name, password, isLogin, e) {
+  e.preventDefault()
 
-setGameUp().play()
+  if(!isLogin) return
+  
+  renderGameScreen()
+  setGameUp().play()
+}
+
+function renderGameScreen () {
+  rootHtmlElement.innerHTML = ""
+  const mainGameDiv = createHtmlElement("div", "main-game")
+
+  const gameDiv = createHtmlElement("div", "genius")
+
+  const redOptionDiv = createHtmlElement("div", "red");
+  const greenOptionDiv = createHtmlElement("div", "green");
+  const blueOptionDiv = createHtmlElement("div", "blue");
+  const yellowOptionDiv = createHtmlElement("div", "yellow");
+
+  gameDiv.appendChild(redOptionDiv)
+  gameDiv.appendChild(greenOptionDiv)
+  gameDiv.appendChild(blueOptionDiv)
+  gameDiv.appendChild(yellowOptionDiv)
+
+  mainGameDiv.appendChild(gameDiv)
+  rootHtmlElement.appendChild(mainGameDiv)
+}
+
+
+
 
 
 
